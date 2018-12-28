75210e623e147e1701e32e985b8d2e1735e32f9f
Patrick Pichler <patri.pichler@gmail.com>
2018-12-28 13:55:52 +0100

    Finished exercise 1_29

--- exercise-1_29.clj
+++ exercise-1_29.clj
@@ -3,12 +3,24 @@
 (defn simpsons-rule [f a b n]
   (let [h (/ (- b a) n)]
 
-    (defn simpsons-rule-iter [k]
-     (cond (not= k n) (+ (f (+ a (* k h))) 
-                          (simpsons-rule-iter (inc k)))
-            :else (f (+ a (* k h)))))
+    (defn yk [k] (f (+ a (* h k))))
+
+    (defn get-multiplier [k]
+      (cond (= k 0) 1
+            (= k n) 1
+            (odd? k) 4
+            :else 2))
+
+    (defn simpsons-term [k]
+      (* (get-multiplier k)
+         (yk k)))
+
+    (defn get-simpsons-sum []
+      (->> (range (inc n))
+          (map simpsons-term)
+          (reduce +)))
     
-    (* (/ h 3) (simpsons-rule-iter 0))))
+    (* (/ h 3) (get-simpsons-sum))))
 
 (defn cube [x] (* x x x))
 
